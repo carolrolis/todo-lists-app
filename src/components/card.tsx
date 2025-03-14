6140f463368bb3e01781c9182883e9f3cb1de7ae
Carol <anacarolinasf0101@gmail.com>
2025-03-14 19:56:44 -0300

    an

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -8,11 +8,7 @@ export default function Card({ children }: CardProps) {
 
   return (
     <div className="group m-auto max-w-50 p-2 pb-0 rounded-lg bg-purple-500 hover:bg-purple-700 hover:cursor-pointer">
-<<<<<<< HEAD
       <img alt={name} src={`../public/assets/card-images/${name.toLowerCase()}.png`}></img>
-=======
-      <img alt={name} src={`/src/assets/card-images/${name.toLowerCase()}.png`}></img>
->>>>>>> 0e72436c55028d0db9a1d548bf28d6f845aea1f1
       <button className="w-full mt-2 mb-2 text-white text-center font-medium" title="card" >
         {name}
       </button>
